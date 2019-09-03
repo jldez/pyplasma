b6abf7c945f968eeb582519ba11b637472dfa4be
Jean-Luc Déziel <jean-luc.deziel@leddartech.com>
2019-09-03 13:55:49 -0400

    Update propagation1d example

--- examples/propagation1d.py
+++ examples/propagation1d.py
@@ -4,12 +4,13 @@ import numpy as np
 import pyplasma as pp
 
 
-time = np.linspace(-20e-15, 40e-15, 3000)
-las = pp.Laser(wavelength=800e-9, pulse_duration=10e-15, fluence=2e4, t0=time.min(), phase=True)
-mat = pp.Material(index=2.0, bandgap=5e-19, density=2e28, cross_section=1e-19, damping=1e15, resonance=120e-9, rate_equation='sre')
-dom = pp.Domain(2000e-9, Nx=100, pml_width=800e-9, lasers=[{'x':0,'laser':las}], materials=[{'x_min':300e-9,'x_max':1700e-9,'material':mat}])
+time = np.linspace(-20e-15, 40e-15, 4000)
+las = pp.Laser(wavelength=800e-9, pulse_duration=10e-15, fluence=5e3, t0=time.min(), phase=True, pos=0)
+mat = pp.Material(index=1.0, bandgap=5e-19, density=2e28, cross_section=1e-19, damping=1e15, resonance=120e-9, rate_equation='dre')
+dom = pp.Domain(1000e-9, Nx=40, pml_width=800e-9, Laser=las, materials=[{'x_min':200e-9,'x_max':800e-9,'material':mat}])
 
-results = pp.propagate(time, dom, output=['rho','electric_field','bounded_current'], remove_pml=True, accelerate_fi=True, progress_bar=True)
+results = pp.propagate(time, dom, output=['rho','electric_field','bounded_current'], \
+					   out_step=5, remove_pml=True, accelerate_fi=True, progress_bar=True)
 print(results['rho'].max())
 
 
@@ -29,7 +30,7 @@ def animate(i):
 			line.set_data(dom.x, results['rho'][i]/results['rho'].max())
 	return lines
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-							   frames=len(time), interval=10/len(time)*1e3, blit=True)
+							   frames=int(len(time)/5), interval=10/len(time)*5*1e3, blit=True)
 
 
 plt.show()
