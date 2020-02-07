9593025a94b32605af060ad89138779f56c72eaa
jean-luc <jluc1011@hotmail.com>
2020-02-06 21:20:09 -0500

    Trying lipss simulation (WIP)

--- examples/new_api_test.py
+++ examples/new_api_test.py
@@ -6,11 +6,11 @@ import scipy.constants as c
 def example_3D():
   set_backend('torch.cuda')
 
-  time = Time(start=0, end=40*fs, Nt=5e3)
+  time = Time(start=0, end=40*fs, Nt=3e3)
 
-  dom = Domain(grid=[250,150,150], size=[1*um,3*um,3*um], pml_width=200*nm)
+  dom = Domain(grid=[100,250,250], size=[1*um,5*um,5*um], pml_width=200*nm)
 
-  laser = Laser(wavelength=800*nm, pulse_duration=10*fs, fluence=4.7e4, t0=25*fs, phase=True)
+  laser = Laser(wavelength=800*nm, pulse_duration=10*fs, fluence=4.7e4, t0=20*fs, phase=True)
   dom.add_laser(laser, position='default')
 
   material = Material(
@@ -21,11 +21,11 @@ def example_3D():
                     # ionization_params={'rate_equation':'dre','bandgap':9.*c.e,'density':2e28,'cross_section':1e-19}
                     )
 
-  dom.add_material(material, boundaries={'xmin':250*nm}, roughness=40*nm)
+  dom.add_material(material, boundaries={'xmin':300*nm}, roughness=40*nm)
 
-  dom.add_observer(Observer('Ez', 'watch', x=380*nm, vlim=(-laser.E0*1.1, laser.E0*1.1), keep_pml=True, out_step=3))
-  dom.add_observer(Observer('Ez', 'watch', y=1*um, z=1*um, vlim=(-laser.E0*1.1, laser.E0*1.1), keep_pml=True, out_step=3))
-  dom.add_observer(Observer('rho', 'watch', x=380*nm, vlim=(0,material.density), out_step=3))
+  dom.add_observer(Observer('Ez', 'watch', x=420*nm, vlim=(-laser.E0*1.1, laser.E0*1.1), keep_pml=True, out_step=1))
+  dom.add_observer(Observer('Ez', 'watch', y=1*um, z=1*um, vlim=(-laser.E0*1.1, laser.E0*1.1), keep_pml=True, out_step=1))
+  dom.add_observer(Observer('rho', 'watch', x=420*nm, vlim=(0,material.density), out_step=1))
 
   results = dom.run(time)
 
