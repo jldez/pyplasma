b82b9d2d687ec38cf0c7b1f452c05a8cdbed4351
jean-luc <jluc1011@hotmail.com>
2020-02-08 17:06:09 -0500

    More precise keldysh interpolation

--- pyplasma/field_ionization.py
+++ pyplasma/field_ionization.py
@@ -79,7 +79,7 @@ def fi_rate(E, material, laser, tol=1e-3):
 
 
 # TODO : expose N to API, because it is a major bottleneck to be fine tuned
-def fi_table(material, laser, N=200, tol=1e-3):
+def fi_table(material, laser, N=1000, tol=1e-4):
 
 	Es = np.logspace(3,np.log10(3*laser.E0),int(N))
 	table = bd.zeros((N,2))
